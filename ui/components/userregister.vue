de1c77148978bcf0e6ae165b02dca64c193e1f8f
sajithsurendran <sajithsurendran007@gmail.com>
2023-04-20 16:49:54 +0530

    changes to the ui in the code

--- ui/components/userregister.vue
+++ ui/components/userregister.vue
@@ -1,12 +1,12 @@
 <template>
         <v-container style="width: 40%; padding-top: 5% ;">
-                <h1 class="text-center text-indigo-600 p-3 font-bold text-2xl">User Registration</h1>
+                <h1 class="text-center text-white p-3 font-bold text-2xl">User Registration</h1>
                 <v-form>
                     <v-text-field label="Name" v-model="name" :rules="[rules.required,rules.name]" variant="underlined"></v-text-field>
                     <v-text-field label="Email" v-model="email" :rules="[rules.required,rules.email]" variant="underlined"></v-text-field>
                     <v-text-field label="Password" v-model="password" type="password" :rules="[rules.required,rules.password]" variant="underlined"></v-text-field>
                     <v-container style="width: 50%;" class="text-center">
-                        <v-btn @click="signin()" text color="purple-accent-3" class="w-40 h-20">Register</v-btn>
+                        <v-btn @click="signin()" text  class="w-40 h-20">Register</v-btn>
                     </v-container>
                 </v-form>
         </v-container>
--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -3,10 +3,10 @@
     <v-app-bar app color="transparent" elevation="0">
         <NuxtLink to="/" class="text-white px-20 m-4 text-2xl font-bold " > Neutral Zone</NuxtLink>
         <v-spacer></v-spacer>
-        <NuxtLink to="/services" class="p-4 m-4 text-l text-white font-bold hover:bg-fuchsia-500 rounded ">Services</NuxtLink>
-        <NuxtLink to="/about" class="p-4 m-4 text-l text-white font-bold hover:bg-fuchsia-500 rounded ">About</NuxtLink>
-        <NuxtLink to="/contact" class="p-4 m-4 text-l text-white font-bold hover:bg-fuchsia-500 rounded ">Contact</NuxtLink>
-        <NuxtLink to="/register" class="py-10 m-4 text-l text-white font-bold hover:bg-fuchsia-500 rounded ">Register</NuxtLink>
+        <NuxtLink to="/services" class="p-2 m-2 text-l text-white font-sans font-bold ">Services</NuxtLink>
+        <NuxtLink to="/about" class="p-2 m-2 text-l text-white font-sans font-bold ">About</NuxtLink>
+        <NuxtLink to="/contact" class="p-2 m-2 text-l text-white font-sans font-bold">Contact</NuxtLink>
+        <NuxtLink to="/register" class="py-10 mr-20 text-l text-white font-sans font-bold">Register</NuxtLink>
 
     </v-app-bar>
     <v-main class="bg-fuchsia-800">
